47f0141e781d907884e375d791064e406ab655e7
ryohei and go <watanaber17+g.nakano555@gmail.com>
2019-07-30 15:36:32 +0900

    initial setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,21 @@ import "../styles/styles.css";
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = { currentView: "AllPhotos", photos: [], selectedPhoto: "" };
   }
 
+  // componentDidMount() {}
   render() {
     return (
       <div className="app">
         <h1>Hello World!</h1>
+        <Navbar />
+        {this.state.currentView == "AllPhotos" ? (
+          <AllPhotos />
+        ) : (
+          <SinglePhoto />
+        )}
       </div>
-    )
+    );
   }
 }
